a7282cc1a71cac409ca7cb56f2ad367b0e6a93d4
Lee Seung Heon <tmdgjs380@naver.com>
2025-03-04 20:33:03 +0900

    day0

---
+++ JS/Day1/Console/script.js
@@ -0,0 +1,19 @@
+console.log("Hello, World!");
+console.log(123);
+console.log(true);
+
+var greeting = "안녕하세요";
+console.log(greeting);
+
+console.log({ a: "a", b: "b" });
+
+console.log("Error!");
+console.warn("Warning!");
+
+console.time("Hello");
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.timeEnd("Hello");
---
+++ JS/Day1/var,let,const/script.js
@@ -0,0 +1,29 @@
+var greeting = "hello";
+console.log(greeting); // hello
+
+var greeting = "hi";
+console.log(greeting); // hi
+
+greeting = "how are you";
+console.log(greeting); // how are you
+
+console.log("----------------------------");
+
+// 중복 선언 불가, 재할당은 가능
+let greeting2 = "hello";
+console.log(greeting2); // hello
+
+greeting2 = "hi";
+console.log(greeting2); // hi
+
+console.log("----------------------------");
+
+// // 중복 선언 불가, 재할당도 불가
+// const greeting3 = "hello";
+// console.log(greeting3); // hello
+
+// const greeting3 = "hi";
+// console.log(greeting3); // Uncaught SyntaxError: Identifier 'greeting3' has already been declared
+
+// greeting3 = "how are you";
+// console.log(greeting3); // Uncaught TypeError: Assignment to constant variable.
