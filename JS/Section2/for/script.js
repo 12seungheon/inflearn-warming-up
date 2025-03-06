2d53a95fb02106bf8ca4d58e405585798f4cc442
Lee Seung Heon <tmdgjs380@naver.com>
2025-03-06 21:01:42 +0900

    day2

---
+++ JS/Section2/for/script.js
@@ -0,0 +1,35 @@
+// for (let i = 0; i < 10; i++) {
+//   if (i === 3) {
+//     console.log("Three");
+//     continue;
+//   }
+
+//   if (i === 5) {
+//     console.log("Five");
+//     break;
+//   }
+//   console.log("Number" + i);
+// }
+
+// const user = {
+//   name: "John",
+//   province: "경기도",
+//   city: "용인시",
+// };
+
+// for (let x in user) {
+//   console.log(`${x}: ${user[x]}`);
+// }
+
+// let i = 0;
+// while (i < 10) {
+//   console.log("Number" + i);
+//   i++;
+// }
+
+// let i = 0;
+
+// do {
+//   console.log("Number" + i);
+//   i++;
+// } while (i < 10);
---
+++ JS/Section3/window/script.js
@@ -0,0 +1,19 @@
+let val;
+
+// Outer height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+// Inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll points
+val = window.scrollY;
+val = window.scrollX;
+
+// Location object
+val = window.location;
+// window.location.href = "http://google.com";
+
+console.log(val);
