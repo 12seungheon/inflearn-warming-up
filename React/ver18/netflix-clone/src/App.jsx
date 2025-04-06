b0075b197b33ae29829304c842c757d691d4b80f
Lee Seung Heon <tmdgjs380@naver.com>
2025-04-06 17:20:39 +0900

    why

--- React/ver18/netflix-clone/src/App.jsx
+++ React/ver18/netflix-clone/src/App.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import Nav from "./components/Nav";
 import Banner from "./components/Banner";
+import Row from "./components/Row";
+import requests from "./api/requests";
 
 function App() {
   return (
     <div>
       <Nav />
       <Banner />
+
+      <Row
+        title="Netflix Originals"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+      <Row
+        title="Horror Movies"
+        id="HM"
+        fetchUrl={requests.fetchHorrorMovies}
+      />
     </div>
   );
 }
